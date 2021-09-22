737372b3a7433f290080a3d36b22d08bfc78e157
Josh Sanchez <joshuaesanch@gmail.com>
2021-09-21 23:41:57 -0500

    Init bot config object during client creation

--- src/client.ts
+++ src/client.ts
@@ -1,4 +1,5 @@
 import { Client, Intents } from "discord.js";
+import { config } from "dotenv";
 import { Db } from "mongodb";
 
 import cmd_register from "./commands/register";
@@ -6,12 +7,37 @@ import {
   cmd_tenmans,
   handleButton as tenmansHandleButton,
 } from "./commands/tenmans";
+import botConfig from "./config/botConfig";
+
+async function initBotConfig (client: Client, db: Db) {
+  const botConfigCollection = db.collection("config");
+  let botConfigDoc = await botConfigCollection.findOne({
+    "configName": "bot"
+  });
+    
+  if (!!botConfigDoc) {
+    // Persist default configuration data
+    const defaultChannelId = process.env.DEFAULT_QUEUEMSG_CHANNELID || "885704092142428200";
+
+    botConfigDoc = await botConfigCollection.insertOne({
+      "configName": "bot",
+      "queueChannelId": defaultChannelId
+    });
+  }
+
+  const channelId = botConfigDoc.queueChannelId;
+  botConfig.queueMsgChannel = client.channels.cache.get(channelId);
+}
 
 export default (db: Db) => {
   const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-  client.on("ready", () => {
+  client.on("ready", async () => {
     console.log(`Logged in as ${client.user.tag}!`);
+    
+    console.log(`Loading configuration data from db...`);
+    await initBotConfig(this, db);
+    console.log(`Config successfully initialized.`);
   });
 
   client.on("interactionCreate", async (interaction) => {
