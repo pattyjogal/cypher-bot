d8399682ac6f5c1aabf5fdc90fb1226ebe468c95
Josh Sanchez <joshuaesanch@gmail.com>
2021-09-23 00:19:01 -0500

    Format botConfig.ts with prettier

--- src/config/botConfig.ts
+++ src/config/botConfig.ts
@@ -1,9 +1,7 @@
 import { Channel } from "discord.js";
 
 class BotConfig {
-    constructor(
-        public queueMsgChannel ?:Channel
-    ) {}
+  constructor(public queueMsgChannel?: Channel) {}
 }
 
 export default new BotConfig();
